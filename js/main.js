7960eb4d8e866bd62b9b38da89718520a98a3c83
Cindy <cindy2438@gmail.com>
2023-10-26 00:00:34 +0800

    feat: read xml file.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
+let _xml = ''
 
-// status: loading / success / error
+/**
+ * trigger when user uploading xml file
+ */
+$('#upload-input').on('change', ({ target }) => {
+  const file = target.files[0]
+  $(target).val('') // clear uploaded file
+  getDataFromXmlFile(file)
+})
+
+/**
+ * extract data from uploaded file
+ * @param {File} file uploaded xml file
+ */
+const getDataFromXmlFile = (file) => {
+  const reader = new FileReader()
+  reader.onload = ({ target: { result } }) => {
+    _xml = result
+  }
+  reader.readAsText(file)
+}
+
+/**
+ * add a row in status block
+ * @param {string} status loading / success / error
+ * @param {string} text custom message
+ */
 const addStatusRow = ({ status = 'loading', text = '驗證中...' } = {}) => {
   const isLoading = status === 'loading'
   const isSuccess = status === 'success'
