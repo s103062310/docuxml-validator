c5257a3c874bc33b61d02018a887752cdf513b53
cindy.lai <sihpin.lai@bridgewell.com>
2023-10-26 14:15:52 +0800

    chore: add prettier and JSDoc.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,53 @@
-let _xml = ''
+let _originXml = ''
 
 /**
  * trigger when user uploading xml file
+ * @param {JQuery.ChangeEvent} event
  */
-$('#upload-input').on('change', ({ target }) => {
-  const file = target.files[0]
-  $(target).val('') // clear uploaded file
+$('#upload-input').on('change', (event) => {
+  const inputElement = /** @type {HTMLInputElement} */ (event.target)
+  const file = inputElement.files[0]
+  $(inputElement).val('') // clear uploaded file
   getDataFromXmlFile(file)
 })
 
 /**
  * extract data from uploaded file
- * @param {File} file uploaded xml file
+ * @param {File} file
  */
 const getDataFromXmlFile = (file) => {
   const reader = new FileReader()
-  reader.onload = ({ target: { result } }) => {
-    _xml = result
+
+  /**
+   * callback after loading: save result into _originXml and start validating
+   * @param {ProgressEvent<FileReader>} event
+   */
+  reader.onload = (event) => {
+    _originXml = /** @type {string} */ (event.target.result)
+    addStatusRow()
   }
+
   reader.readAsText(file)
 }
 
 /**
+ * @typedef {Object} StatusRow
+ * @property {string} [status] loading / success / error
+ * @property {string} [text] custom message
+ */
+
+/**
+ * stop validation
+ * @param {StatusRow} row row data which is going to replace loading
+ */
+const stopValidation = (row) => {
+  $('.status-row:last-child').remove()
+  addStatusRow(row)
+}
+
+/**
  * add a row in status block
- * @param {string} status loading / success / error
- * @param {string} text custom message
+ * @param {StatusRow} row row data which is going to display
  */
 const addStatusRow = ({ status = 'loading', text = '驗證中...' } = {}) => {
   const isLoading = status === 'loading'
@@ -32,9 +55,9 @@ const addStatusRow = ({ status = 'loading', text = '驗證中...' } = {}) => {
 
   const style = isLoading ? '' : `style="color: var(--color--${status});"`
 
-  const icon = isLoading 
-    ? '<div class="spinner-grow" aria-hidden="true"></div>' 
-    : `<i class="bi bi-${ isSuccess ? 'check' : 'x' }-circle-fill"></i>`
+  const icon = isLoading
+    ? '<div class="spinner-grow" aria-hidden="true"></div>'
+    : `<i class="bi bi-${isSuccess ? 'check' : 'x'}-circle-fill"></i>`
 
   const html = `
     <div class="status-row" ${style}>
